fd624d322b64574a9bf57516f16ce1f631ebcf65
johgee <graceeunaejoh@grace.com>
2022-03-22 17:37:27 -0400

    adding what i did minus part 2 and 3

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,41 @@
 class ContactsController < ApplicationController
-  def one_contact
+  # def one_contact
+  #   render json: contacts.as_json
+  # end 
+
+  def index
+    contacts = Contact.all
     render json: contacts.as_json
   end 
+
+  def create
+    contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email]
+    )
+    contact.save
+    render json: contact.as_json 
+  end
+
+  def show
+    contact = Contact.find_by(id: params[:id])
+    render json: contact.as_json 
+  end
+
+  def update
+    contact = Contact.find_by(id: params[:id])
+    contact.first_name = params[:first_name] || contact.first_name
+    contact.last_name = params[:last_name] || contact.last_name
+    contact.email = params[:email] || contact.email
+    contact.save
+    render json: contact.as_json
+  end 
+
+  def destroy
+    contact = Contact.find_by(id: params[:id])
+    contact.destroy
+    render json: {message: "destroyed."}
+  end 
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
  
-  get "/one_contact", controller: "contacts", action: "get_one_contact"
+  # get "/one_contact", controller: "contacts", action: "get_one_contact"
+
+  get "/contacts" => "contacts#index"
+  post "/contacts" => "contacts#create"
+  get "/contacts/:id" => "contacts#show"
+  patch "/contacts/:id" => "contacts#update"
+  delete "/contacts/:id" => "contacts#destroy"
+
+  #users
+  post "/users" => "users#create"
+
+  post "/sessions" => "sessions#create"
 end
